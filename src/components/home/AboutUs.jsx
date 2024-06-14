3e448eaf2a32e7cb4fb1d4047045836989cfa7b3
kikani01 <satishkikani0@gmail.com>
2024-06-14 11:13:46 +0530

    my-app

---
+++ src/components/home/AboutUs.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function AboutUs(props) {
+   if (props.imageFirst) {  
+       return (
+         <div>
+          <h1 className="text-4xl text-center font-bold text-blue-700"  >{props.title}</h1>
+          <div className="h-[300px] w-[100%] p-7 flex  justify-between  gap-[300px]">
+             <img className="h-full rounded-md" src={props.img}  alt=""/>
+             <div>
+             <p className="font-medium ml-20 ">
+                {props.des} 
+             </p>
+              </div>
+          </div>
+         </div>
+       )
+   } 
+   else{
+    return (
+    <div>
+          <h1 className="text-4xl text-center font-bold text-blue-700" >{props.title}</h1>
+          <div className="h-[300px] w-[100%] p-7 flex  justify-between  mt-5">
+             <div>
+             <p className="font-medium leading-1">
+                {props.des} 
+             </p>
+              </div>
+             <img className="h-full rounded-md" src={props.img}  alt=""/>
+          </div>
+         </div>
+    )
+   }
+}
+
+export default AboutUs
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../components/home/Navbar'
 import Content from '../components/home/Content'
+import AboutUs from '../components/home/AboutUs'
 
 function Home() {
   return (
     <div>
     <Navbar/>
     <Content/>
+    <AboutUs title=" About us" img="https://images.unsplash.com/photo-1689602037070-fec2eca3f5b2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" des="Lorem ipsum dolor sit amet, consectetur elit. Amet excepturiiendis corporis consequuntur ipsum earum. Quaerat, voluptatibus" imageFirst={true}
+    />
+
+    <AboutUs title=" our story" img="https://images.unsplash.com/photo-1689602037070-fec2eca3f5b2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" des="Lorem ipsum dolor sit amet, consectetur elit. Amet excepturiiendis corporis consequuntur ipsum consectetur eli." />
     </div> 
 
   )
